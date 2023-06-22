6441aec5ff787034772a92244ff098bdb279df57
tagomenonoka <20239@toba-cmt.ac.jp>
2023-06-22 21:14:15 +0900

    changes css

--- app/app_body/profile/page.tsx
+++ app/app_body/profile/page.tsx
@@ -26,47 +26,53 @@ export default function Profile() {
                 {/* アカウント主のicon */}
                 <div> <Image src={user.icon} alt='アイコン' width={100} height={100} /></div>
                 {/* アカウントの内容 */}
-                <div className='font-AnekGujarati text-backgray text-[27px] font-extrabold line-height-[41px]'>{user.name}</div>
+                <div className='font-AnekGujarati text-backgray text-[27px] font-extrabold line-height-[41px]'>
+                 <div className="flex items-center h-full">
+                    {user.name}
+                </div>
+                </div>
             </div>
-        <div className="flex flex-row justify-end space-x-6">
-            <div>
-                <span>フォロワー</span>
-                <span>{user.follower}</span>
+            <div className="flex flex-row justify-end space-x-6">
+                <div>
+                    <span>フォロワー</span>
+                    <span>{user.follower}</span>
+                </div>
+                <div>
+                    <span>フォロー中</span>
+                    <span>{user.follow}</span>
+                </div>
+                <div></div>
             </div>
-            <div>
-                <span>フォロー中</span>
-                <span>{user.follow}</span>
-            </div>
-            <div></div>
-        </div>    
-        <div>
-            <div>
-                {/* プロフィール編集へ */}
-                    <button
-                        onClick={() => {
-                            router.push('./profile/profile_edit')
-                        }}
-
-                        //目視できるように追加しただけなので変更してください
-                        className='fixed t-500'
-                    >
-                        プロフィールを編集
-                    </button>
-
-                    {/* ログアウトボタン */}
-                    <button
-                        onClick={() => {
-                            router.push('/')
-                        }}
-
-                        //目視できるように追加しただけなので変更してください
-                        className='fixed t-500'
-                    >
-
-                        <LogoutIcon />
-
+            <div className='flex flex-row justify-end space-x-6 m-2'>
+                <div>
+                    {/* プロフィール編集へ */}
+                    <button style={{ 
+                        backgroundColor: 'rgba(221, 223, 228, 1)', 
+                        color: 'rgba(99, 123, 152, 1)', fontSize: '11px', fontFamily: 'Anek Gujarati',
+                        width: '123px', height: '26px', borderRadius: '5px'
+                    }}  
+                        onClick={() => { 
+                            router.push('./profile/profile_edit') }}>
+                    プロフィールを編集
                     </button>
                 </div>
+                <div>
+                    {/* ログアウトボタン */}
+                    <button style={{
+                        backgroundColor: 'rgba(221, 223, 228, 1)',
+                        color: 'rgba(99, 123, 152, 1)',
+                        width: '49px', height: '26px', borderRadius: '5px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                        onClick={() => {
+                            router.push('/')}}>
+
+                    <LogoutIcon />
+                    </button>
+                </div>
+                <div></div>
 
             </div>
 
