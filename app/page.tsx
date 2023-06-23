1852625c36c3bf416b05c5f8a7f1405cc57d169d
tagomenonoka <20239@toba-cmt.ac.jp>
2023-06-23 12:47:18 +0900

    changes css

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ import Signup from './components/signup'
 export default function Index() {
 
   //component切り替え
-  const [switching,setSwiching]=useState({
+  const [switching, setSwiching] = useState({
 
     //ログインとサインアップ
-    'login_signup':'login'
+    'login_signup': 'login'
 
   })
 
@@ -22,14 +22,15 @@ export default function Index() {
     <div>
 
       {/* タイトル表示部分 */}
-      <div>
-
+      <div className='w-full h-64 relative'>
+        <div className='w-full h-64 top-[0px] absolute' style={{ backgroundColor: 'rgba(221, 223, 228, 1)', }}></div>
         {/* タイトルを挿入 */}
         <Image
-          src = "/pado_title.png"
-          alt = "ぱどろーる"
-          width = { 250 }
-          height = { 150 }
+          src="/pado_title.png"
+          alt="ぱどろーる"
+          width={250}
+          height={250}
+          className='w-full h-[138px]  flex justify-center top-[101px] absolute'
         />
 
       </div>
@@ -38,23 +39,23 @@ export default function Index() {
       <div>
 
         {/* ログインと新規登録で分ける */}
-        {switching.login_signup=='login'?
-        <Login
+        {switching.login_signup == 'login' ?
+          <Login
 
-        //新規アカウント作成を押したときの処理
-        to_signup = {()=>{
-          setSwiching({...switching,'login_signup': 'signup' })
-        }}
-        />
-        :
-        <Signup
-        //新規アカウント作成を押したときの処理
-        to_login = {()=>{
-          setSwiching({...switching,'login_signup': 'login' })
-        }}
-        />
+            //新規アカウント作成を押したときの処理
+            to_signup={() => {
+              setSwiching({ ...switching, 'login_signup': 'signup' })
+            }}
+          />
+          :
+          <Signup
+            //新規アカウント作成を押したときの処理
+            to_login={() => {
+              setSwiching({ ...switching, 'login_signup': 'login' })
+            }}
+          />
         }
-        
+
 
       </div>
 
