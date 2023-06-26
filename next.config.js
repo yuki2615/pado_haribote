7dbe4a90aab5e35394b29b132368059005acb716
yuki2615 <yuki.matsuba105@gmail.com>
2023-06-26 14:35:29 +0900

    workboxをinstall

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withPWA = require("next-pwa")({
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
 });
 
