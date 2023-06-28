e037ba4c214adbf93bc8193ea213c1df03564b7f
takara2314 <takara2314_TK@outlook.jp>
2023-06-28 19:18:29 +0900

    Set: PWA

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
-/** @type {import('next').NextConfig} */
-module.exports = {};
+const {
+  PHASE_PRODUCTION_BUILD
+} = require('next/constants');
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = (phase) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require('@ducanh2912/next-pwa').default({
+      dest: 'public',
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
